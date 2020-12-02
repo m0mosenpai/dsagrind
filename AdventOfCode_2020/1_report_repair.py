1918dba9e0c9305b69dca703b727a8fb115b6ce4
Sarthak <sarthakoct@gmail.com>
2020-12-02 18:28:51 +0530

    AoC 2020 Day 1 and 2

---
+++ AdventOfCode_2020/1_report_repair.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+# O(nlog n) Time Complexity | O(1) Space complexity
+# Best sorting algorithm would take O(nlog n)
+def part_1(data_list):	
+	start = 0
+	end = len(data_list) - 1
+	_sum = 0
+
+	while start < end:
+		_sum = data_list[start] + data_list[end]
+
+		if _sum < 2020:
+			start += 1
+		elif _sum > 2020:
+			end -= 1
+		else:
+			print(f"{data_list[start]} + {data_list[end]} = {_sum}. Product = {data_list[start]*data_list[end]}")
+			exit(0)
+
+# O(n^2) Time Complexity | O(1) Space Complexity
+# Best sorting algorithm would take O(nlog n)
+def part_2(data_list):
+	for start in range(len(data_list)):
+		mid = start + 1
+		end = len(data_list) - 1
+		_sum = 0
+
+		while mid <= end:
+			_sum = (data_list[start] + data_list[mid] + data_list[end])
+			if _sum < 2020:
+				mid += 1
+			elif _sum > 2020:
+				end -= 1
+			else:
+				print(f"{data_list[start]} + {data_list[mid]} + {data_list[end]} = {_sum}. Product = {data_list[start]*data_list[mid]*data_list[end]}")
+				exit(0)
+
+if __name__ == '__main__':
+	with open("1.txt", "r") as f:
+		data = f.read()
+
+	data_list = list(map(int, data.strip().split("\n")))
+	data_list.sort()
+
+	part_1(data_list)
+	part_2(data_list)
