5e6dbd45058acf5199339c468e640dec352b3086
Sarthak Khattar <sarthakoct@gmail.com>
2025-02-16 17:24:07 -0600

    added day-6 part-1

--- aoc_2024/day_6/6.go
+++ aoc_2024/day_6/6.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"os"
     "strings"
+    // "time"
 )
 
 var x, y int
 var maze = make([]string, 0)
+var maze_map = make(map[dir]string)
 var curr_dir dir
 var positions int = 0
+var player string = "^"
 
 type dir struct {
     x int
@@ -23,24 +26,50 @@ var dir_map = map[dir]dir{
     {x: 0, y: -1}: {x: -1, y: 0},
 }
 
+var player_map = map[string]string{
+    "^" : ">",
+    ">" : "v",
+    "v" : "<",
+    "<" : "^",
+}
+
 func move(i *int, j *int) {
+    maze_map[dir{x: *i, y: *j}] = "X"
     *i += curr_dir.x
     *j += curr_dir.y
+    maze_map[dir{x: *i, y: *j}] = player
 }
 
 func turnRight() {
-    curr_dir.x = dir_map[curr_dir].x
-    curr_dir.y = dir_map[curr_dir].y
+    curr_dir = dir_map[curr_dir]
+    player = player_map[player]
 }
 
 func checkObstacle(i int, j int) bool {
-    next_i, next_j := i + curr_dir.x, j + curr_dir.y
-    if ((next_i < x && next_i >= 0) &&
-        (next_j < y && next_j >= 0) &&
-        string(maze[next_i][next_j]) == "#") {
+    if ((i < x && i >= 0) && (j < y && j >= 0) && maze_map[dir{x: i, y: j}] == "#") {
         return true
     }
     return false
+}
+
+func createMazeMap(x int, y int) {
+    for i := range x {
+        for j := range y {
+            maze_map[dir{x: i, y: j}] = string(maze[i][j])
+        }
+    }
+}
+
+func renderMaze() {
+    for i := range x {
+        for j := range y {
+            fmt.Printf("%s", maze_map[dir{x: i, y: j}])
+        }
+        fmt.Println()
+    }
+    fmt.Printf("Positions: %d\n\n", positions)
+    // time.Sleep(500 * time.Millisecond)
+    // fmt.Print("\033[H\033[2J")
 }
 
 
@@ -54,47 +83,47 @@ func main() {
     maze = strings.Split(data, "\n")
 
     x, y = len(maze) - 1, len(maze[0])
-    visited := make([][]int, x)
-    for i := range visited {
-        visited[i] = make([]int, y)
-    }
+    var visited = make(map[dir]int)
+    createMazeMap(x, y)
 
     // facing up in the start
     curr_dir.x = -1
     curr_dir.y = 0
 
     // get start position
-    var g_x, g_y int
-    for i := range maze {
-        for j := range maze[i] {
-            if string(maze[i][j]) == "^" {
-                g_x = i
-                g_y = j
-                visited[i][j] = 1
-                positions++
-                break
-            }
+    var start dir
+    for c, v := range(maze_map) {
+        if v == "^" {
+            start.x = c.x
+            start.y = c.y
+            visited[c] = 1
+            positions++
+            break
         }
     }
+    fmt.Printf("Starting Position: (%d, %d)\n", start.x, start.y)
 
     // traverse
-    var i, j int = g_x, g_y
-    for i > 0 && i < x && j > 0 && j < y {
+    var i, j int = start.x, start.y
+    for i >= 0 && i < x && j >= 0 && j < y {
+        // display maze
+        // renderMaze()
+
         // mark current pos as visited
-        if visited[i][j] != 1 {
+        if visited[dir{x: i, y: j}] != 1 {
             positions++
         }
-        visited[i][j] = 1
+        visited[dir{x: i, y: j}] = 1
 
         // check for obstacles
-        if checkObstacle(i, j) {
+        if checkObstacle(i + curr_dir.x, j + curr_dir.y) {
             turnRight()
-            fmt.Println("Turned Right!")
+        } else {
+            // step ahead
+            move(&i, &j)
         }
 
-        // step ahead
-        move(&i, &j)
-        fmt.Printf("Moved to i: %d, j: %d\n", i, j)
     }
+    renderMaze()
     fmt.Println("Part-1:", positions)
 }
